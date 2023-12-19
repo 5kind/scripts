f4c07f1685bf0b1740c698b4a761f5f5bdd22a70
5kind <weirui5671@outlook.com>
2023-12-19 16:57:53 +0800

    skip backup: cache

--- backup-by-name.sh
+++ backup-by-name.sh
@@ -5,10 +5,11 @@ basedata=$(basename $(realpath /dev/block/by-name/userdata))
 
 for block in /dev/block/by-name/* ;do
 	baseblock=$(basename $block)
-	if [ $baseblock != userdata ] && ! echo "$basedata" | grep -q "${baseblock}[0-9]*$" ;then
+	if [ $baseblock != userdata ] && [ $baseblock != cache ] && \
+    ! echo "$basedata" | grep -q "${baseblock}[0-9]*$" ;then
         echo "$block -> $bakdir/$baseblock.img"
 	    dd if=$block of=$bakdir/$baseblock.img
     else
-        echo "Skip the backup of $block"
+        echo "Skip backup: $block"
     fi
 done
